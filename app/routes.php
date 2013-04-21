484e48778a9b9a15472b51417d0ec10f2ce0a8e1
Andy Awidarto <andy.awidarto@gmail.com>
2013-04-22 00:15:13 +0700

    setup auth

---
+++ app/routes.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+Route::get('/', array('as'=>'home', function()
+{
+	return View::make('home');
+}));
+
+Route::get('login', array('as' => 'login', function () {
+	return View::make('login');
+}))->before('guest');
+
+Route::post('login', function () { 
+
+    $user = array(
+        'email' => addslashes(Input::get('username')),
+        'password' => Input::get('password')
+    );
+    
+    if (Auth::attempt($user)) {
+        return Redirect::route('home')
+            ->with('flash_notice', 'You are successfully logged in.');
+    }
+    
+    // authentication failure! lets go back to the login page
+    return Redirect::route('login')
+        ->with('flash_error', 'Your username/password combination was incorrect.')
+        ->withInput();
+
+});
+
+Route::get('signup',function(){
+	return View::make('register');
+});
+
+Route::post('signup',function(){
+
+	$id = LMongo::collection('users')->insert(
+	    array('username' => Input::get('username'),'email' => Input::get('email'), 'password' => Hash::make(Input::get('password')))
+	);	
+
+	if($id){
+	    return Redirect::to('signup')
+	        ->with('flash_notice', 'Registration Complete')
+	        ->withInput();
+    }else{
+	    return Redirect::to('signup')
+	        ->with('flash_error', 'Registration Failed')
+	        ->withInput();
+    }
+});
+
+Route::get('logout', array('as' => 'logout', function () { 
+	Auth::logout();
+    return Redirect::route('home')
+        ->with('flash_notice', 'You are successfully logged out.');
+
+}))->before('auth');
